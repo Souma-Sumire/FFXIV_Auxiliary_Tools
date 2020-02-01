0db7e5dfb86b81ad9c192ae8d1b806b89951c34f
553469159 <553469159@qq.com>
2020-02-02 02:15:45 +0800

    Fix bug
    修复了路径中含有空格会报错的问题

--- 添加当前目录到环境变量.bat
+++ 添加当前目录到环境变量.bat
@@ -13,5 +13,6 @@ exit /B
 setlocal enabledelayedexpansion
 set tempPath=%~dp0
 set tempPath=!tempPath:~,-1!
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v SoumaTriggerFolder /t REG_SZ /d %tempPath% /f
+echo %tempPath%
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v SoumaTriggerFolder /t REG_SZ /d "%tempPath%" /f
 pause
