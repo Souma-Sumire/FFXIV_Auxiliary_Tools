4f52d018f47b7697966a26cc42530144aa60613f
553469159 <553469159@qq.com>
2020-02-07 03:35:45 +0800

    各个触发器分离

--- 添加触发器目录与鲶鱼精邮差端口到环境变量.bat
+++ 添加触发器目录与鲶鱼精邮差端口到环境变量.bat
@@ -4,4 +4,5 @@ set tempPath=%~dp0
 set tempPath=!tempPath:~,-1!
 echo %tempPath%
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v SoumaTriggerFolder /t REG_SZ /d "%tempPath%" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PostNamazu /t REG_SZ /d "2019" /f
 pause
