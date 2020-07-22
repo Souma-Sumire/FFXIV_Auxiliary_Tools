4ae9561f7d9106404c0f3499dfa1c8456cbecc05
Souma <553469159@qq.com>
2020-07-22 22:24:53 +0800

    Update

--- 配置环境变量.bat
+++ 配置环境变量.bat
@@ -1,9 +1,5 @@
 @echo off
 %1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" ::","","runas",1)(window.close)&&exit
-setlocal enabledelayedexpansion
-set tempPath=%~dp0
-set tempPath=!tempPath:~,-1!
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v SoumaTriggerFolder /t REG_SZ /d "%tempPath%" /f
 set input=2019
 set /p input=输入鲶鱼精邮差监听端口（留空则为2019）:
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PostNamazu /t REG_SZ /d "%input%" /f
